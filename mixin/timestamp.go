113d37cbe6835db2cc2c264697b4f06bbe0b0dad
bearchit <bearchit@gmail.com>
2021-06-06 08:50:31 +0900

    chore: cleanup timestamp mixin
    - add annotations to mixin/timestamp
    - remove legacy entgql package

--- mixin/timestamp.go
+++ mixin/timestamp.go
@@ -3,6 +3,8 @@ package mixin
 import (
 	"time"
 
+	"entgo.io/contrib/entgql"
+
 	"entgo.io/ent"
 	"entgo.io/ent/schema/field"
 	"entgo.io/ent/schema/mixin"
@@ -16,9 +18,11 @@ func (Timestamp) Fields() []ent.Field {
 	return []ent.Field{
 		field.Time("created_at").
 			Immutable().
-			Default(time.Now),
+			Default(time.Now).
+			Annotations(entgql.OrderField("CREATED_AT")),
 		field.Time("updated_at").
 			Default(time.Now).
-			UpdateDefault(time.Now),
+			UpdateDefault(time.Now).
+			Annotations(entgql.OrderField("UPDATED_AT")),
 	}
 }
